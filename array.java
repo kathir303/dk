deaf0fb5adefd62eedec2acc08b609f8d5d479cf
kathir303 <rmadirdfc@gmail.com>
2025-05-02 14:19:41 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,14 @@
+public class array {
+    public static void main(String[] args){
+        int[] arr = {1,2,3};
+         System.out.println("Subarrays of the given array:");
+         for (int i = 0; i < arr.length; i++) {
+            for(int j = i; j < arr.length; j++){
+                for( int k = i; k <=j; k++) {
+                    System.out.println(arr[k] + " ");
+
+                }
+            }
+         }
+    }
+} 
